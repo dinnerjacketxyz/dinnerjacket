3153213df797410c5517dac1a0e6c249da5a7b22
Gregaly <gaf0161@outlook.com>
2017-12-18 00:39:26 +1100

    Added min nav bar, animations, icons

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,6 +10,20 @@ window.STATES = {
   SETTINGS: 1
 }
 
+const W_DEFAULT = '150px'
+const W_MIN = '52px'
+
+let navbarMin = false // Based on cookie from previous visit in future
+const NAV_COLOURS = ['red', 'blue', 'purple']
+const NAV_TEXT = ['Main', 'Settings', 'Theme']
+let btnNavDefault = []
+let btnNavMin = []
+
+for (let i = 0; i < NAV_COLOURS.length; i++) {
+  btnNavDefault[i] = 'fluid ui labeled icon ' + NAV_COLOURS[i] + ' button'
+  btnNavMin[i] = 'ui compact icon ' + NAV_COLOURS[i] + ' button'
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -17,10 +31,32 @@ class App extends Component {
     // Set default state on launch
     this.state = {visible: window.STATES.INFO}
   }
+  
+  toggleNavbar = () => {
+    console.log('navbar toggled')
+    let nav = document.getElementById('navbar')
+    let content = document.getElementById('content')
+    let btnNav1 = document.getElementById('btnNav1')
+    let btnNav2 = document.getElementById('btnNav2')
+    let btnNav3 = document.getElementById('btnNav3')
+    
+    if (navbarMin) {
+      nav.style.width = W_DEFAULT
+      content.style.marginLeft = W_DEFAULT
+    } else {
+      nav.style.width = W_MIN
+      content.style.marginLeft = W_MIN
+    }
+
+    toggleButton(btnNav1, 0)
+    toggleButton(btnNav2, 1)
+    toggleButton(btnNav3, 2)
+    navbarMin = !navbarMin
+  }
 
   showInfo = () => {
     const {visible} = this.state
-    this.setState({visible: window.STATES.INFO}) //TODO change to window state vars
+    this.setState({visible: window.STATES.INFO})
   }
 
   showSettings = () => {
@@ -28,35 +64,39 @@ class App extends Component {
     this.setState({visible: window.STATES.SETTINGS})
   }
 
+  // Always renders navbar
+  // Renders active page
   render() {
+    let nav = document.getElementById('navbar')
     return (
       <div id='main' className='light'>
         <div id='navbar' className='navbarLight'>
 
           <div className='sidebarIcon'>
-            <button className='ui compact icon button'>
+            <button className='ui compact icon button' onClick={this.toggleNavbar}>
               <i className='sidebar icon' />
             </button>
+            <img className='sidebarLogo' type='image/png' />
           </div>
 
           <div className='navButtons'>
-            <button className='fluid ui labeled icon red button' onClick={this.showInfo}>
-              Main
+            <button className={btnNavDefault[0]} id='btnNav1' onClick={this.showInfo}>
+              {NAV_TEXT[0]}
               <i className='wait icon' />
             </button>
             <p/>
-            <button className='fluid ui labeled icon blue button' onClick={this.showSettings}>
-              Settings
+            <button className={btnNavDefault[1]} id='btnNav2'onClick={this.showSettings}>
+              {NAV_TEXT[1]}
               <i className='settings icon' />
             </button>
             <p/>
-            <button className='fluid ui labeled icon purple button' onClick={this.toggleTheme}>
-              Theme
+            <button className={btnNavDefault[2]} id='btnNav3' onClick={this.toggleTheme}>
+              {NAV_TEXT[2]}
               <i className='theme icon' />  
             </button>
           </div>
         </div>
-        <div className='content'>
+        <div className='content' id='content'>
           {this.state.visible === window.STATES.INFO && <Info />}
           {this.state.visible === window.STATES.SETTINGS && <Settings />}
         </div>
@@ -64,6 +104,7 @@ class App extends Component {
     )
   }
 
+  // Toggles display theme between light and dark
   toggleTheme() {
     let mainClass = document.getElementById('main').className
     let navClass = document.getElementById('navbar').className
@@ -79,4 +120,17 @@ class App extends Component {
   }
 }
 
+function toggleButton(button, index) {
+  if (navbarMin) {
+    button.className = btnNavDefault[index]
+    button.childNodes[0].nodeValue = NAV_TEXT[index]
+  } else {
+    button.className = btnNavMin[index]
+    button.childNodes[0].nodeValue = ''
+  }
+}
+
 export default App
+
+// TODO apply navbar margin-left in css based on 
+// variables W_DEFAULT and W_MIN in this file
