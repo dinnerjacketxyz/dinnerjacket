c60f2b151e398bfefe496cd0640d783733456cef
brian <briannguyen013@gmail.com>
2018-02-18 01:58:38 +1100

    adjusted navbar height, added icon functionality on Microsoft Edge, added design for the beta release of the dashboard, minor css tweaks, better commit messages for marks

--- app/Components/App.js
+++ app/Components/App.js
@@ -152,10 +152,12 @@ class App extends Component {
   render() {
     return (
       <div id='main' className='main'>
-        <nav className='uk-navbar uk-navbar-container uk-margin' uk-navbar='true'>
-          <div className='uk-navbar-left'>      
-            <ul id='navbar' className='uk-navbar-nav'>
+        <nav className='uk-navbar uk-navbar-container uk-margin' uk-navbar='true' height='70px'>
 
+          <div className='uk-navbar-left'>
+            <img id='logo' className='uk-disabled uk-margin-small-left uk-margin-small-right uk-margin-small-top uk-margin-small-bottom' alt='logo' src='https://i.imgur.com/xxuf1ni.png' width='60px' height='50px'/>
+            <ul id='navbar' className='uk-navbar-nav'>
+              
               <li id='DashboardLi' className='uk-animation-toggle uk-active' onClick={this.showDashboard.bind(this)}>
                 <a id='DashboardA' className='uk-box-shadow-hover-medium uk-card-primary'>
                   <span className='uk-icon uk-margin-small-right' uk-icon='icon: home'/>
