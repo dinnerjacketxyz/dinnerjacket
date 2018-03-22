817d586ee28163f75940271ffde7a4332f80d209
brian <briannguyen013@gmail.com>
2018-03-22 21:53:06 +1100

    loading page fix

--- app/Components/Loading.js
+++ app/Components/Loading.js
@@ -4,9 +4,9 @@ class Loading extends Component {
   render() {
     return (
       <div id='main' className='main uk-height-viewport'>
-        <div uk-spinner="ratio: 4" class="uk-spinner uk-icon">
+        <div uk-spinner="ratio: 4" className="uk-spinner uk-icon">
           <svg width="120" height="120" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg" ratio="4">
-            <circle fill="none" stroke="#000" cx="15" cy="15" r="14" style="stroke-width: 0.25px;"></circle>
+            <circle fill="none" stroke="#000" cx="15" cy="15" r="14"></circle>
           </svg>
         </div>
       </div>
