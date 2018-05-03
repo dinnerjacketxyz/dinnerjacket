1311299be441807e82d74a2f422c39f94c65208b
nguyenbrian <briannguyen013@gmail.com>
2018-05-03 16:25:29 +1000

    minor

--- server/auth.js
+++ server/auth.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
           |   REPLACE THIS WITH CLIENT SECRET WHEN RUNNING   |
           *==================================================*
                                                                 */
-      secret: REDACTED
+      secret: 'lve26aPJH_zzKPHBUrVAcpIGhjQ'
 
       /*
           *==================================================*
