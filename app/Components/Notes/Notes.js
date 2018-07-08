e1db9fcbf2b38a586f99c5405a731945ddda0a6e
nguyenbrian <briannguyen013@gmail.com>
2018-07-08 18:19:47 +1000

    notes ui for multiple notes and the ability to add them

--- app/Components/Notes/Notes.js
+++ app/Components/Notes/Notes.js
@@ -62,6 +62,22 @@ class Notes extends Component {
     return (
       <div className='vcNavbarCard notesParent'>
         <div className='notesChild card uk-animation-slide-top-small'>
+          <ul className="uk-subnav uk-subnav-pill uk-flex-center" uk-switcher="animation: uk-animation-fade">
+              <li><a>Item</a></li>
+              <li><a>Item</a></li>
+              <li><a>Item</a></li>
+              <li>
+                <a>Add <span className="uk-margin-small-left" uk-icon="icon: plus-circle"></span></a>
+                <div uk-dropdown="mode: click">
+                  <p>Item</p>
+                </div>
+              </li>
+          </ul>
+          <ul className="uk-switcher uk-margin">
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+              <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, sed do eiusmod.</li>
+          </ul>
           <div className='pad'>
             <div id='editor' onInput={this.updateDB.bind(this)}/>
           </div>
